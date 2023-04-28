4384381b77617c7bea49ce570e0f5a170ba9e46f
Jakob <shavinski.jakob@gmail.com>
2023-04-28 15:43:51 -0700

    set up add pet form and validation

--- app.py
+++ app.py
@@ -2,10 +2,11 @@
 
 import os
 
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
 
 from models import connect_db, db, Pet
+from forms import AddPetForm
 
 app = Flask(__name__)
 
@@ -30,5 +31,31 @@ def show_homepage():
     pets = Pet.query.all()
 
     return render_template("home.html", pets=pets)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_pet():
+    """ Handles getting the add route and the form validation for adding a pet """
+
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        name = form.name.data or None
+        species = form.species.data or None
+        photo_url = form.photo_url.data or None
+        age = form.age.data or None
+        notes = form.notes.data or None
+
+        pet = Pet(name=name, species=species, photo_url=photo_url, age=age, notes=notes)
+
+        db.session.add(pet)
+        db.session.commit()
+
+        flash(f"Added {name} the {species} to the adoption page")
+        return redirect('/')
+
+    else:
+        return render_template(
+            "add-pet-form.html", form = form)
+
 
     
--- forms.py
+++ forms.py
@@ -1,1 +1,31 @@
 """Forms for adopt app."""
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField
+from wtforms.validators import InputRequired, Optional, URL
+
+class AddPetForm(FlaskForm):
+    """ Form for adding pets """
+
+    name = StringField(
+        "Pet name",
+        validators=[InputRequired()])
+
+    species = StringField(
+        "Species",
+        validators=[InputRequired()])
+
+    photo_url = StringField("Photo URL", 
+        validators=[Optional(), URL()])
+        #SHOULD WE HAVE OPTIONAL HERE OR JUST LEAVE IT OUT
+
+    age = SelectField(
+        "Age",
+        choices = [('baby', 'Baby'), ('young', 'Young'), ('adult', 'Adult'), ('senior', 'Senior')], 
+        validators=[InputRequired()])
+
+    notes = StringField(
+        "Notes", 
+        validators=[Optional()])
+        #SHOULD WE HAVE OPTIONAL HERE OR JUST LEAVE IT OUT
+
+
